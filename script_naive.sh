b9864c156d5ccdca8a4ad48af342b06e833cba84
Rottschaefer <rottschaefer54@gmail.com>
2026-04-26 12:35:00 -0300

    Mudanca de nome e ressultados com 1 recv  do naive

--- script_naive.sh
+++ script_naive.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 LIMITE=500000000
-PROGRAMA="./main"
-ARQUIVO_SAIDA="resultados_bag.csv"
+PROGRAMA="./primos_naive"
+ARQUIVO_SAIDA="resultados_naive.csv"
 
 echo "modo_send,tempo_em_segundos" > $ARQUIVO_SAIDA 
 
-mpicc -o main main.c -lm
+mpicc -o primos_naive primos_naive.c -lm
 
 echo "Programa Compilado"
 echo "----------------------"
 echo "Iniciando os testes"
 
-for SEND_MODE in 1 2 4 5
+for SEND_MODE in 1 2 3 4 5
 do  
     for RECV_MODE in 1
     do
         echo "Rodando com modo Send-$SEND_MODE e Recv-$RECV_MODE"
         SAIDA=$(mpirun -n 4 --oversubscribe $PROGRAMA $LIMITE $SEND_MODE $RECV_MODE)
 
+        echo "$SAIDA"
         
         TEMPO=$(echo "$SAIDA" | grep "Tempo de execucao:" | awk '{print $4}')
         echo "$SEND_MODE,$TEMPO" >> $ARQUIVO_SAIDA
