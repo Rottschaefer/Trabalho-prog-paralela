9358d1376c62721e37b0a1ee3a6cdbb0661e695e
Rottschaefer <rottschaefer54@gmail.com>
2026-04-26 22:55:59 -0300

    Relatorio atualizado com dados da execucao com Irecv

--- script_naive.sh
+++ script_naive.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Configurações
 LIMITE=500000000
 PROGRAMA="./primos_naive"
-ARQUIVO_SAIDA="resultados_naive.csv"
+ARQUIVO_SAIDA="resultados_naive_irecv.csv"
+TEMPO_1_PROC=676.828
+NUM_PROCS=4
 
-echo "modo_send,modo_recv,tempo_em_segundos" > $ARQUIVO_SAIDA
+echo "modo_send,modo_recv,tempo_em_segundos,speedup,eficiencia" > $ARQUIVO_SAIDA
 
 mpicc -o primos_naive primos_naive.c -lm
 
@@ -14,7 +17,7 @@ echo "Iniciando os testes"
 
 for SEND_MODE in 1 2 3 4 5
 do  
-    for RECV_MODE in 1 2
+    for RECV_MODE in 2
     do
         echo "Rodando com modo Send-$SEND_MODE e Recv-$RECV_MODE"
         SAIDA=$(mpirun -n 4 --oversubscribe $PROGRAMA $LIMITE $SEND_MODE $RECV_MODE)
@@ -22,7 +25,17 @@ do
         echo "$SAIDA"
         
         TEMPO=$(echo "$SAIDA" | grep "Tempo de execucao:" | awk '{print $4}')
-        echo "$SEND_MODE,$RECV_MODE,$TEMPO" >> $ARQUIVO_SAIDA
+        
+        # Só calcula se o valor de TEMPO for válido/existir
+        if [[ ! -z "$TEMPO" ]]; then
+            SPEEDUP=$(awk "BEGIN {printf \"%.3f\", $TEMPO_1_PROC / $TEMPO}")
+            EFICIENCIA=$(awk "BEGIN {printf \"%.3f\", $SPEEDUP / $NUM_PROCS}")
+        else
+            SPEEDUP="-"
+            EFICIENCIA="-"
+        fi
+        
+        echo "$SEND_MODE,$RECV_MODE,$TEMPO,$SPEEDUP,$EFICIENCIA" >> $ARQUIVO_SAIDA
         echo "--------------------------------------"
     done
 done
