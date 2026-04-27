9681f334d4804ebbe213e732d6cfe653b9244002
Marcus Bossan <mvsbossan@id.uff.br>
2026-04-26 21:13:17 -0300

    Implementação do MPI_Irecv e novo nome no "PROGRAMA"

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 LIMITE=500000000
-PROGRAMA="./main"
+PROGRAMA="./primos_bag"
 ARQUIVO_SAIDA="resultados_bag.csv"
 
-echo "modo_send,tempo_em_segundos" > $ARQUIVO_SAIDA 
+echo "modo_send,modo_recv,tempo_em_segundos" > $ARQUIVO_SAIDA 
 
-mpicc -o main main.c -lm
+mpicc -o primos_bag primos_bag.c -lm
 
 echo "Programa Compilado"
 echo "----------------------"
@@ -14,14 +14,14 @@ echo "Iniciando os testes"
 
 for SEND_MODE in 1 2 4 5
 do  
-    for RECV_MODE in 1
+    for RECV_MODE in 1 2
     do
         echo "Rodando com modo Send-$SEND_MODE e Recv-$RECV_MODE"
         SAIDA=$(mpirun -n 4 --oversubscribe $PROGRAMA $LIMITE $SEND_MODE $RECV_MODE)
 
         
         TEMPO=$(echo "$SAIDA" | grep "Tempo de execucao:" | awk '{print $4}')
-        echo "$SEND_MODE,$TEMPO" >> $ARQUIVO_SAIDA
+        echo "$SEND_MODE,$RECV_MODE,$TEMPO" >> $ARQUIVO_SAIDA
         echo "--------------------------------------"
     done
 done
